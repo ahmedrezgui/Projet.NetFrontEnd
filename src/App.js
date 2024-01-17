b55150f2d794311a7a76213713a7a3b06eb7fb1f
rezgu <rezgui012@gmail.com>
2024-01-17 18:55:18 +0100

    added profile page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from './profile';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/login">
-          </Route>
+          <Route path="/login" />
+          <Route path='/profile' element={<Profile />} />
+
         </Routes>
       </Router>
     </>
