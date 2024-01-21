218e2e82466a34dfed08319fd4e7c1c30061db47
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-01-21 15:41:03 +0100

    add login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './Components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login">
+        <Route path='/login' element={<Login></Login>}>
         </Route>
       </Routes>
     </Router>
