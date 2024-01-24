8c5d7ea7ff34fce5fe4d7f66ccc3c60c7f973416
AzizBenDhiab <mohamedaziz.bendhiab@insat.ucar.tn>
2024-01-24 12:42:14 +0100

    fixing app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CheckEvents from './Components/checkEvents';
+import AddWork from './Components/AddWork';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login">
-        </Route>
+        <Route path="/login"> </Route>
+        <Route path="/events" Component={CheckEvents}></Route>
+        <Route path="/Addevents" Component={AddWork}></Route>
+
       </Routes>
     </Router>
   );
