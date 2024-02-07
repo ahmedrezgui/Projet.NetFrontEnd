56ed28e2ddf2755c807fb3bef1320e32cecd1d02
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2024-02-07 23:36:42 +0100

    add member functionality

--- src/Components/AddMember.js
+++ src/Components/AddMember.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import '../Style/addEvents.css';
 import { Form, Button } from 'react-bootstrap';
 import MembersSelect from './membersSelect';
+import { useEffect } from 'react';
+import {redirect} from "react-router-dom";
+
 
 
 
@@ -9,12 +12,39 @@ const AddTasks = (props) => {
 
    const selectedMembers=props.selectedMembers;
    const setSelectedMembers=props.setSelectedMembers;
-    const [description, setDescripion] = useState('');
-    const [name, setName] = useState('');
-    const [day, setDay] = useState('jj/mm/aaaa');
-    const [formErrors, setFormErrors] = useState({});
+  const [generalError, setGeneralError] = useState('');
+  const [generalSuccess, setGeneralSuccess] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [address, setAddress] = useState('');
+    const [isAdmin, setIsAdmin] = useState(false); // assuming default is false
+    const [formErrors, setFormErrors] = useState({
+        lastName: '',
+        firstName: '',
+        email: '',
+        password: '',
+        phoneNumber: '',
+        address: '',
+        // Add other fields here for validation errors
+    });
 
-    const handleSelectChange = (memberId) => {
+    useEffect(() => {
+        let timeoutId;
+        if (generalSuccess !== '') {
+            timeoutId = setTimeout(() => {
+                setGeneralSuccess('');
+            }, 4000); // 30 seconds
+        }
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [generalSuccess]);
+
+        const handleSelectChange = (memberId) => {
         // Check if the memberId is already in selectedMembers
         if (selectedMembers.includes(memberId)) {
             // If yes, remove it
@@ -29,66 +59,134 @@ const AddTasks = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setGeneralError('')
+        setGeneralSuccess('')
+        let nbrerror=0;
+        const errors = {
+            lastName: '',
+            firstName: '',
+            email: '',
+            password: '',
+            phoneNumber: '',
+            address: '',
 
-        // Perform validation
-        const errors = {};
+        };
 
-        if (!name.trim()) {
-            errors.name = 'Name is required';
+        if (!lastName.trim()) {
+            errors.lastName = 'Last name is required';
+            nbrerror++;
+        }
+        if (!firstName.trim()) {
+            errors.firstName = 'First name is required';
+            nbrerror++;
+        }
+        if (!email.trim()) {
+            errors.email = 'Email is required';
+            nbrerror++;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            errors.email = 'Email is invalid';
+            nbrerror++;
+        }
+        if (!password.trim()) {
+            errors.password = 'Password is required';
+            nbrerror++;
+        }else if(password.length<6){
+            errors.password = 'Password must be at least 6 characters long';
+            nbrerror++;
+        }
+        if (!phoneNumber.trim()) {
+            errors.phoneNumber = 'Phone number is required';
+            nbrerror++;
+        }else if (!/^\d{8}$/.test(phoneNumber)) {
+            errors.phoneNumber = 'Phone number is invalid';
+            nbrerror++;
+        }
+        if (!address.trim()) {
+            errors.address = 'Address is required';
+            nbrerror++;
+        }else if (!/^[a-zA-Z0-9\s,.'-]{3,}$/.test(address)) {
+            errors.address = 'Address is invalid';
+            nbrerror++;
         }
 
-        if (day==="jj/mm/aaaa") {
-            errors.day = 'Day is required';
-        }
-
-        if (!description.trim()) {
-            errors.description = 'Details are required';
-        }
 
 
+        if (nbrerror === 0) {
+            const userData = {
+               "LastName": lastName,
+                "FirstName": firstName,
+                "Email": email,
+                "PasswordHash": password,
+                "PhoneNumber": phoneNumber,
+                "Adress": address,
+                "IsAdmin": isAdmin
+            };
 
-        // Add more validation as needed
 
-        if (Object.keys(errors).length === 0) {
-            const users = selectedMembers.map((value, index) => (value));
 
             let token=localStorage.getItem('JwtToken');
-            const taskData = {
-                name,
-                deadLine: `${day}T23:59:59:00Z`,
-                description,
-                users
-            };
-            console.log(taskData);
+
 
             try {
-                const response = await fetch('https://localhost:7181/Task/Create', {
+                const response = await fetch('https://localhost:7181/Auth/register', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                         'Authorization': 'bearer ' + token,
                     },
-                    body: JSON.stringify(taskData),
+                    body: JSON.stringify(userData),
                 });
 
+
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                   if(response.status===409) {
+
+                       errors.email = 'Email already exists';
+
+
+
+                       throw new Error('Email already exists');
+                   }else if(response.status===401){
+                       window.location.href = '/login';
+                   }
+                   else {
+
+                       throw new Error('Network response was not ok');
+                   }
+                }else {
+                    setGeneralSuccess('User added successfully')
+                    setLastName('');
+                    setFirstName('');
+                    setEmail('');
+                    setPassword('');
+                    setPhoneNumber('');
+                    setAddress('');
+                    setIsAdmin(false);
                 }
 
-                console.log('Event data posted successfully!');
+
                 // Handle any additional actions upon successful submission
             } catch (error) {
-                console.error('Error posting event data:', error);
+                if (error.name === 'AbortError') {
+                     setGeneralError('Request aborted')
+                    console.error('Request aborted:', error);
+                } else if (error.message === 'Failed to fetch') {
+                   setGeneralError('Network error: Failed to fetch')
+                    console.error('Network error: Failed to fetch');
+                } else {
+                    if(errors.email!=='Email already exists'){
+                        setGeneralError('Error posting event data:')
+                        console.error('Error posting event data:', error);
+                    }
+
+                }
             }
-            setSelectedMembers([]);
-            setDescripion('');
-            setName('');
-            setDay('jj/mm/aaaa');
-            setFormErrors({});
-        } else {
-            // Update state with validation errors
-            setFormErrors(errors);
+
+
         }
+
+        setFormErrors(errors);
+
     };
 
     return(
@@ -99,50 +197,112 @@ const AddTasks = (props) => {
                 <MembersSelect members={props.members}
                                selectedMembers={selectedMembers}
                                handleSelectChange={handleSelectChange}
+                               selectStyle={"70vh"}
                 />
 
 
 
-            <div className='right-panel-2'>
-                {/* Text Input */}
-                <Form.Group controlId="name">
-                    <Form.Label>Insert Task Name</Form.Label>
-                    <Form.Control type="text"  placeholder="Enter some text" className='input'  value={name}
-                                  onChange={(e) => setName(e.target.value)}/>
-                    <div className='error' >{formErrors.name && <div className="error">{formErrors.name}</div>}
-                    </div>
+            <div className='right-panel-2' style={{height:"70vh"}}>
+                <Form.Group controlId="lastname">
+                    {generalError!=='' ? <div style={{color:"red"}}>{generalError}</div>: ""}
+                    {generalSuccess!=='' ? <div style={{color:"green"}}>{generalSuccess}</div>: ""}
+                    <Form.Label style={{ marginTop: generalError !== ''||generalSuccess !=='' ? "1vh" : null }}>{formErrors.lastName !== '' ? <div style={{color:"red"}}>{formErrors.lastName}</div>: "Insert Last Name"}</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Your Last Name"
+                        className='input'
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="firstname">
+                    <Form.Label>{formErrors.firstName !=='' ?
+                            <div style={{color:"red"}}>{formErrors.firstName}</div>
+                        : "Insert First Name"}</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Your First Name"
+                        className='input'
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="email">
+                    <Form.Label>{formErrors.email !== '' ?
+                        <div style={{color:"red"}}>{formErrors.email}</div>:  "Email"}</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Your Email"
+                        className='input'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
                 </Form.Group>
 
-                {/* Text Input */}
-                <Form.Group  controlId="description">
-                    <Form.Label >Insert Task Details </Form.Label>
-                    <Form.Control type="text" placeholder="Enter some text" className='input' value={description}
-                                  onChange={(e) => setDescripion(e.target.value)}/>
-                    <div className='error' >{formErrors.description && <div className="error">{formErrors.description}</div>}
+                <Form.Group controlId="password">
+                    <Form.Label>{formErrors.password !== "" ?
+                        <div style={{color:"red"}}>{formErrors.password}</div> : "Password"}</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Your Password"
+                        className='input'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+
+                </Form.Group>
+
+                <Form.Group controlId="phoneNumber">
+                    <Form.Label>{formErrors.phoneNumber !== "" ?
+                        <div style={{color:"red"}}>{formErrors.phoneNumber}</div> : "Phone Number"}</Form.Label>
+                    <Form.Control
+                        type="tel"
+                        placeholder="Your Phone Number"
+                        className='input'
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+
+                </Form.Group>
+
+                <Form.Group controlId="address">
+                    <Form.Label>{formErrors.address !== "" ?
+                        <div className="error">{formErrors.address}</div> : "Address"}</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Your Address"
+                        className='input'
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+
+                </Form.Group>
+
+                <Form.Group controlId="isAdmin" className="d-flex align-items-center" >
+                    <div className="col-auto"  >
+                        <Form.Check
+                            style={{ transform: 'scale(1.2)' ,marginTop:"2vh"}}
+                            type="checkbox"
+                            checked={isAdmin}
+                            onChange={(e) => setIsAdmin(e.target.checked)}
+                        />
+                    </div>
+                    <div className="col-auto">
+                        <Form.Label  style={{ transform: 'scale(1.1)' ,marginTop:"1.5vh",marginLeft:"1vw"}} className="mb-0">Is Admin</Form.Label>
                     </div>
                 </Form.Group>
 
-                {/* Day Input */}
-                <Form.Group controlId="day" className='day-container'>
-                    <Form.Label>Insert Task Deadline</Form.Label>
-                    <Form.Control type="date" placeholder="Select a day" className='input' value={day}
-                                  onChange={(e) => setDay(e.target.value)}/>
-                    <div className='error' > {formErrors.day && <div className="error">{formErrors.day}</div>}
-                    </div>
-                </Form.Group>
+
+                <Button variant="primary" type="submit" className='buttonform' style={{marginTop:"5.5vh",backgroundColor:"#1D995E",border:"0px"}}>
+                    Add
+                </Button>
 
 
-
-
-
-                {/* Submit Button */}
-                <div className='centered-button-container '>
-                    <Button variant="primary" type="submit" className='buttonform' >
-                        Confirm
-                    </Button>
-                </div>
             </div>
+
         </Form>
 
 
--- src/Components/AddRemoveMember.js
+++ src/Components/AddRemoveMember.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import AddMember from "./AddMember";
+import {  Button } from 'react-bootstrap';
 
 
 
@@ -28,16 +29,20 @@ const AddWork = () => {
                 });
                 // Check if the response is successful
                 if (!response.ok) {
+                    if(response.status===401){
+                        window.location.href = '/login';
+                    }
                     throw new Error('Network response was not ok');
                 }
 
                 // Parse the JSON data
                 const data = await response.json();
 
-                // Update the state with the fetched events
+
 
                 setAllMembers(data);
                 setMembers(data);
+
             } catch (error) {
                 console.error('Error fetching events:', error);
             }
@@ -64,6 +69,18 @@ const AddWork = () => {
             // Check if the response is successful
             if (response.status!==204) {
                 throw new Error('Network response was not ok');
+            }else {
+                let mmbr=[];
+                allMembers.map((member)=>{if(!selectedMembers.includes(member.id)){
+                    mmbr.push(member)
+                }
+
+
+                })
+                setAllMembers(mmbr)
+                setMembers(mmbr)
+                setSelectedMembers([]);
+
             }
 
         } catch (error) {
@@ -104,27 +121,37 @@ const AddWork = () => {
 
                 <Container className='Big-container' style={{
                     width: "100vh",
+                    height: "100vh",
                     marginLeft: "5vh",
                     borderRadius: "3vh",
                     backgroundColor: "rgb(235, 235, 235)"
                 }}>
-                    <form className="search-form" >
+                    <form className="search-form">
                         <div className="input-wrapper">
-                            <input type="text" placeholder="Search..." value={searchValue} onChange={handleInputChange}/>
+                            <input type="text" placeholder="Search..." value={searchValue}
+                                   onChange={handleInputChange}/>
                             <div className="icon-placeholder">
                                 <FontAwesomeIcon icon={faSearch}/>
                             </div>
-                            <button style={{borderRadius:"5vw",height:"5vh",width:"7vw",marginLeft:"1vw",textAlign:"center",fontWeight:"bold",paddingBottom:"2vw"}} onClick={(e)=>{e.preventDefault(); handleDelete()}}>Delete</button>
                         </div>
                     </form>
                     <div className='conteneur'>
 
                         <div className='left-panel-1'>
-                            Select Members w
+                            Delete Members
+                        </div>
+                        <div className='left-panel-1' style={{marginLeft:"3vw",width:"51%"}}>
+                          <span style={{marginLeft:"5vw"}}>Add A Member</span>
                         </div>
 
                     </div>
-                  <AddMember members={members} selectedMembers={selectedMembers} setSelectedMembers={setSelectedMembers}></AddMember>
+                    <AddMember members={members} selectedMembers={selectedMembers}
+                               setSelectedMembers={setSelectedMembers}></AddMember>
+                    <Button variant="primary" className='buttonform' style={{marginTop:"0", marginLeft:"5.3vw",backgroundColor:"#A61E37",border:"0px"}} onClick={(e) => {
+                        e.preventDefault();
+                        handleDelete()
+                    }}>Delete
+                    </Button>
 
                 </Container>
             </div>
